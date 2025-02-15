b1336c6aa207aa5f5d2826e66820d6878f7ad99e
Jrl224 <62444898+Jrl224@users.noreply.github.com>
2025-02-14 17:05:37 -0800

    Create WordCloud.tsx

---
+++ src/components/visualizations/WordCloud.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WordCloud from 'react-d3-cloud';
+
+interface WordCloudProps {
+    data: { text: string; value: number }[];
+}
+
+const WordCloudComponent: React.FC<WordCloudProps> = ({ data }) => {
+    return <WordCloud data={data} />;
+};
+
+export default WordCloudComponent;
