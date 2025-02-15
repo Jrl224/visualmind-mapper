c57a817c4f069aa4f0ffc5f24ddf0b806a3d9514
Jrl224 <62444898+Jrl224@users.noreply.github.com>
2025-02-14 17:04:29 -0800

    Create MindMap.tsx

---
+++ src/components/visualizations/MindMap.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MindMap as ReactMindMap } from 'react-mindmap';
+
+interface MindMapProps {
+    data: any;
+}
+
+const MindMap: React.FC<MindMapProps> = ({ data }) => {
+    return <ReactMindMap data={data} />;
+};
+
+export default MindMap;
